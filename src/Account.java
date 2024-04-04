707df87b6ec0c669e3e1556d1ebc855524750ad9
OSdy55y-Wr1t3r <159829427+OSdy55y-Wr1t3r@users.noreply.github.com>
2024-04-04 15:03:37 -0500

    Merge remote-tracking branch 'origin/master'

--- src/Account.java
+++ src/Account.java
@@ -7,35 +7,79 @@ public class Account {
 
         String databaseURL = "";
 
-        Scanner finput = new Scanner(System.in);
-        String FirstName = finput.next();
-        Scanner linput = new Scanner(System.in);
-        String LastName = linput.next();
-        Scanner uinput = new Scanner(System.in);
-        String UserName = uinput.next();
-        Scanner pinput = new Scanner(System.in);
-        String Password = pinput.next();
 
 
 
-        try( Connection connection = DriverManager.getConnection(databaseURL) ) {
-                String sql = "INSERT INTO Account (FirstName, LastName, ) VALUES (?, ?)";
+        System.out.println("Do you have an account?");
+        System.out.println("Type: \n  login \n  create");
+
+        Scanner loginput = new Scanner(System.in);
+        String login = loginput.next();
+
+        if (login == "login") {
+            try( Connection connection = DriverManager.getConnection(databaseURL) ) {
+                String sql = "SELECT";
+
+                //enter username
+                //search username
+                //"username not found"
+                //enter password
+                
+
+                connection.close();
+            } //try end
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            } //catch end
+
+        }
+        if (login == "create") {
+
+            //set name
+            System.out.println("Please enter your first name");
+            Scanner finput = new Scanner(System.in);
+            String FirstName = finput.next();
+            System.out.println("Please enter your last name");
+            Scanner linput = new Scanner(System.in);
+            String LastName = linput.next();
+
+            //set username
+            System.out.println("Choose a username");
+            Scanner uinput = new Scanner(System.in);
+            String UserName = uinput.next();
+
+            //set password
+            System.out.println("Choose a password");
+            Scanner pinput = new Scanner(System.in);
+            String Password = pinput.next();
+
+
+            try( Connection connection = DriverManager.getConnection(databaseURL) ) {
+                String sql = "INSERT INTO Account (FirstName, LastName, UserName, Password) VALUES (?, ?)";
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setString(1, finput.next());
-                preparedStatement.setString(2, linput.next());
-                preparedStatement.setString(3, uinput.next());
-                preparedStatement.setString(4, pinput.next());
+                preparedStatement.setString(1, FirstName);
+                preparedStatement.setString(2, LastName);
+                preparedStatement.setString(3, UserName);
+                preparedStatement.setString(4, Password);
                 int row = preparedStatement.executeUpdate();
                 if( row > 0 ) {
-                    System.out.println("A row has been inserted");
+                    System.out.println("account has been created");
                 } //if end
 
-            preparedStatement.close();
-            connection.close();
-        } //try end
-        catch (SQLException ex) {
-                    ex.printStackTrace();
-        } //catch end
+                preparedStatement.close();
+                connection.close();
+            } //try end
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            } //catch end
+
+        }
+
+
+
+
+
+
 
 
     } // main end
---
+++ src/NewMultArray.java
@@ -0,0 +1,18 @@
+public class NewMultArray {
+    public static void main(String[] args) {
+
+        int[][] array1 = {
+                {1,2,3},
+                {4,5,6}
+        };
+
+        System.out.println( array1[1][1] );
+
+
+        int[] array2 ={
+                1, 2, 3, 4
+        };
+
+
+    }
+}
